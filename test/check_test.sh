603e9d0e59fb8f8d40e4739c7b2fd119b94fb8a4
Mario Manno <mario.manno@suse.com>
2018-08-22 12:01:33 +0200

    Return empty version list if regex does not match any file

--- test/check_test.sh
+++ test/check_test.sh
@@ -5,24 +5,48 @@
 # kernel-default-4.9.9-1.2.x86_64.rpm
 
 BEFORE="4.9.9-1.2"
-LATEST="4.9.10-1.1"
+LATEST="4.17.14-1.2"
 
-echo "expects single version if given can't be found: the last one $LATEST"
-bash assets/check <<EOF
+echo "expect empty array if regex does not match"
+out=`bash assets/check <<EOF
 {
   "source": {
-    "url": "https://download.opensuse.org/tumbleweed/repo/oss/suse/x86_64/",
+    "url": "https://download.opensuse.org/tumbleweed/repo/oss/x86_64/",
+    "regexp": "kernel-defaultnothere-(.*).x86_64.rpm"
+  },
+  "version": { "ref": "MISSING" }
+}
+EOF
+`
+if ! echo "$out" | grep -q "\[\]"; then
+  echo "expected: []"
+  echo "actual: $out"
+  echo "test failed!!!"
+  exit 1
+fi
+
+echo "expects single version if given can't be found: the last one $LATEST"
+out=`bash assets/check <<EOF
+{
+  "source": {
+    "url": "https://download.opensuse.org/tumbleweed/repo/oss/x86_64/",
     "regexp": "kernel-default-(.*).x86_64.rpm"
   },
   "version": { "ref": "MISSING" }
 }
 EOF
+`
+if ! echo "$out" | grep -q "$LATEST"; then
+  echo "$out"
+  echo "Test failed"
+fi
+
 
 echo "expects two version: [ $BEFORE, $LATEST ]"
 bash assets/check <<EOF
 {
   "source": {
-    "url": "https://download.opensuse.org/tumbleweed/repo/oss/suse/x86_64/",
+    "url": "https://download.opensuse.org/tumbleweed/repo/oss/x86_64/",
     "regexp": "kernel-default-(.*).x86_64.rpm"
   },
   "version": { "ref": "$BEFORE" }
@@ -33,7 +57,7 @@ echo "expects single version if given the current one: $LATEST"
 bash assets/check <<EOF
 {
   "source": {
-    "url": "https://download.opensuse.org/tumbleweed/repo/oss/suse/x86_64/",
+    "url": "https://download.opensuse.org/tumbleweed/repo/oss/x86_64/",
     "regexp": "kernel-default-(.*).x86_64.rpm"
   },
   "version": { "ref": "$LATEST" }
@@ -44,7 +68,7 @@ echo "expects single version if not given any: latest $LATEST"
 bash assets/check <<EOF
 {
   "source": {
-    "url": "https://download.opensuse.org/tumbleweed/repo/oss/suse/x86_64/",
+    "url": "https://download.opensuse.org/tumbleweed/repo/oss/x86_64/",
     "regexp": "kernel-default-(.*).x86_64.rpm"
   }
 }
